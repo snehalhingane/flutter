7df971d46ed91d6e5ec3c8c8590cea9f8724dbc1
snehal hingane <48429365+snehalhingane@users.noreply.github.com>
2020-07-29 14:52:01 +0530

    commit 2

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:avtar_app/ui/home.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myapp1();
+  }
+}
